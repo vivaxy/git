c6e6f4e633f08f5ddc97994af8c0420cf3e9e514
vivaxy <xyxuye2007@126.com>
2019-07-31 15:19:01 +0800

    test(test): :white_check_mark: fix branch test case

--- src/branch/__tests__/get-current.ts
+++ src/branch/__tests__/get-current.ts
@@ -3,10 +3,21 @@
  * @author vivaxy
  */
 import * as path from 'path';
+import * as execa from 'execa';
+import * as fse from 'fs-extra';
 import getCurrentBranch from '../get-current';
 
-const projectBase = path.join(__dirname, '..', '..', '..');
+const wd = path.join(__dirname, 'fixtures', 'git-repo');
+
+beforeAll(async function() {
+  await fse.ensureDir(wd);
+  await execa('git', ['init'], { cwd: wd });
+});
+
+afterAll(async function() {
+  await fse.remove(wd);
+});
 
 test('get current branch', async function() {
-  expect(await getCurrentBranch({ cwd: projectBase })).toBe('master');
+  expect(await getCurrentBranch({ cwd: wd })).toBe('master');
 });
