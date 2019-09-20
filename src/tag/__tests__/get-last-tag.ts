a57cfa843db08b9c0f788594c42d7e3fc79b1518
vivaxy <xyxuye2007@126.com>
2019-09-20 16:51:20 +0800

    feat(tag): get-tags
    add

---
+++ src/tag/__tests__/get-last-tag.ts
@@ -0,0 +1,14 @@
+/**
+ * @since 20180911 17:16
+ * @author vivaxy
+ */
+import * as path from 'path';
+import getLastTag from '../get-last-tag';
+
+const projectBase = path.join(__dirname, '..', '..', '..');
+
+test('get last tag', async function() {
+  expect(
+    await getLastTag({ from: 'd236e911', cwd: projectBase }),
+  ).toStrictEqual('v0.1.8');
+});
---
+++ src/tag/__tests__/get-tags.ts
@@ -0,0 +1,14 @@
+/**
+ * @since 20180911 17:16
+ * @author vivaxy
+ */
+import * as path from 'path';
+import getTags from '../get-tags';
+
+const projectBase = path.join(__dirname, '..', '..', '..');
+
+test('get tags', async function() {
+  const tags = await getTags({ cwd: projectBase });
+  console.log('tags', tags);
+  expect(tags.includes('v0.2.0')).toBe(true);
+});
