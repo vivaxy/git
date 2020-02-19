50aa333d47e0cc525d9e2e639fe466d7b436cf62
vivaxy <xyxuye2007@126.com>
2020-02-19 15:10:07 +0800

    test(get-remote-url): :white_check_mark:  update test cases

--- src/remote/__tests__/get-remote-url.ts
+++ src/remote/__tests__/get-remote-url.ts
@@ -3,26 +3,12 @@
  * @author vivaxy
  */
 import * as path from 'path';
-import * as fse from 'fs-extra';
-import * as execa from 'execa';
 import getRemoteUrl from '../get-remote-url';
 
-const wd = path.join(__dirname, 'fixtures', 'get-remote-url');
-
-beforeAll(async function() {
-  await fse.ensureDir(wd);
-  await execa('git', ['clone', 'https://github.com/vivaxy/git.git'], {
-    cwd: wd,
-  });
-});
-
-afterAll(async function() {
-  await fse.remove(wd);
-});
+const projectPath = path.join(__dirname, '..', '..', '..');
 
 test('get remote url', async function() {
-  const gitRepoPath = path.join(wd, 'git');
-  expect(await getRemoteUrl({ cwd: gitRepoPath, remote: 'origin' })).toBe(
+  expect(await getRemoteUrl({ cwd: projectPath, remote: 'origin' })).toBe(
     'https://github.com/vivaxy/git.git',
   );
 });
