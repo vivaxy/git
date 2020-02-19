8b99fddcdad20630f9141dbb854e4ce77133ea25
vivaxy <xyxuye2007@126.com>
2020-02-19 13:58:48 +0800

    feat(get-rev-count): :sparkles:  add `leftOnly` option

--- src/rev-list/get-rev-count.ts
+++ src/rev-list/get-rev-count.ts
@@ -7,11 +7,13 @@ import { betterExeca } from '../helpers';
 export default async function getRevCount({
   from,
   to = 'HEAD',
+  leftOnly = false,
   paths,
   cwd,
 }: {
   from?: string;
   to?: string;
+  leftOnly: boolean;
   paths?: string[];
   cwd: string;
 }): Promise<number> {
@@ -20,6 +22,9 @@ export default async function getRevCount({
     sha = to;
   }
   let args = ['rev-list', '--count', sha];
+  if (leftOnly) {
+    args.push('--left-only');
+  }
   if (paths) {
     args.push('--', ...paths);
   }
