f30f05c5fefdce93420175cba052086d83107fb8
vivaxy <xyxuye2007@126.com>
2020-05-05 22:34:00 +0800

    chore(status): export `isInsideWorkTree` and `getRootPath`

--- src/status/index.ts
+++ src/status/index.ts
@@ -6,3 +6,5 @@ export { default as getStagedFiles } from './get-staged-files';
 export { default as getHeadCommit } from './get-head-commit';
 export { default as isClean } from './is-clean';
 export { default as isRepositoryRoot } from './is-repository-root';
+export { default as isInsideWorkTree } from './is-inside-work-tree';
+export { default as getRootPath } from './get-root-path';
