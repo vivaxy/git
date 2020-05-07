9e7fb7e7819fb481bb9290b085d97d46cdd40357
vivaxy <xyxuye2007@126.com>
2020-05-07 19:45:01 +0800

    test(commit): :white_check_mark: add `get-previous-commit` testcase

---
+++ src/commit/__tests__/get-previous-commit.ts
@@ -0,0 +1,14 @@
+/**
+ * @since 2020-05-07 19:42
+ * @author vivaxy
+ */
+import getPreviousCommit from '../get-previous-commit';
+import * as project from '../../__tests__/helpers/project';
+
+test('get previous commit', async function() {
+  const previousCommit = await getPreviousCommit({
+    from: 'aebd444f9ab3384d388ac7d0db38f86feaf21a19',
+    cwd: project.rootPath,
+  });
+  expect(previousCommit).toBe('4fa1e6ccf87974320954e9d9dd87904700907a45');
+});
